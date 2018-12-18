dd82d3a056e835bcf862eda16bd1710eb2fc404b
Seungje Jun <ninanung@naver.com>
2018-12-18 11:32:11 +0900

    server and frontedn setting

--- server/app.js
+++ server/app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var connectHistoryApiFallbsck = require('connect-history-api-fallback');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -13,6 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(connectHistoryApiFallbsck())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
